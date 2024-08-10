cc9695b4fed26c84aaae49434b3194f45ec18731
ankita-chowdhury <108832208+ankita-chowdhury@users.noreply.github.com>
2024-08-10 14:09:13 +0530

    setting up local storage and create user

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 const SignUp = () => {
+    const navigate = useNavigate();
+    const[userData,setUserData]=useState([]);
+    const[user,setUser]=useState({
+        username: "",
+        email: "",
+        password: "",
+        dateOfBirth: "",
+        address: "",
+        contactNo: ""
+    })
+
+    const handleChange = (inputvalue,inputName) =>{
+        if(inputName==='username'){
+            setUser((olditems)=>{
+                return{...olditems,username:inputvalue};
+            })
+        }
+        else if(inputName==='email'){
+            setUser((olditems)=>{
+                return{...olditems,email:inputvalue};
+            })
+        }
+        else if(inputName==='password'){
+            setUser((olditems)=>{
+                return{...olditems,password:inputvalue};
+            })
+        }
+    }
+
+    const handleSignUp = async() =>{
+        try{
+            const tempUserData=await axios.get(`http://localhost:4500/users?email=${user.email}`);
+            if(tempUserData.data.length===0){
+                const response=await axios.post(`http://localhost:4500/users`,user);
+            console.log("response",response);
+            const localData={user:user.username,email:user.email};
+            localStorage.setItem('loginKey',JSON.stringify(localData));
+            navigate('/home');
+            }
+            else{
+                console.log("user already exist!");
+            }
+        } 
+        catch(e){
+            console.log(e);
+        }
+    }
+
   return (
     <div className="outer-container">
         <div className="inner-container">
@@ -11,18 +60,18 @@ const SignUp = () => {
             <div className="login-form">
                     <label htmlFor="username">Name</label>
                     <div className="input-div">
-                    <input type="text" name="username" id="username" placeholder="Enter your name"/>
+                    <input type="text" name="username" id="username" placeholder="Enter your name" onChange={(e)=>handleChange(e.target.value,'username')}/>
                     </div>
                     <label htmlFor="email">Email address</label>
                     <div className="input-div">
-                    <input type="email" name="email" id="email" placeholder="Enter your email"/>
+                    <input type="email" name="email" id="email" placeholder="Enter your email" onChange={(e)=>handleChange(e.target.value,'email')}/>
                     </div>
                     <label htmlFor="password">Password</label>
                     <div className="input-div">
-                    <input type="password" name="password" id="password" placeholder="Enter your password"/>
+                    <input type="password" name="password" id="password" placeholder="Enter your password" onChange={(e)=>handleChange(e.target.value,'password')}/>
                     </div>
                     <div className="login-btn">
-                        <button>Sign Up</button>
+                        <button onClick={()=>handleSignUp()}>Sign Up</button>
                         <p>Already registered? <Link to='/login'>Login</Link></p>
                     </div>
             </div>
