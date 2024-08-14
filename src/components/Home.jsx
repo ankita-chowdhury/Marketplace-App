066e0803aeaa135602517c93e834f13a719a4507
ankita-chowdhury <108832208+ankita-chowdhury@users.noreply.github.com>
2024-08-14 20:15:11 +0530

    Implemented Product list item

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect} from 'react'
 import { useNavigate } from 'react-router';
 import Navbar from './Navbar';
+import ProductCard from './ProductCard';
+import axios from 'axios';
 
 const Home = () => {
     const navigate = useNavigate();
+    const[productItem,setProductItem]=useState([]);
     useEffect(()=>{
         const loginKey=localStorage.getItem('loginKey');
         if(!loginKey){
           navigate('/login');
         }
-      },[])
+        productApiCall();
+      },[productItem])
+
+      const productApiCall = async() =>{
+        try{
+          const response = await axios.get(`http://localhost:4500/products`);
+          setProductItem(response.data);
+        }
+        catch(e){
+          console.log(e);
+          
+        }
+      }
+
   return (
     <div className='box-container'>
       <Navbar/>
-      Home
+      <div className="welcome-msg"><h2>Welcome Back!</h2></div>
+      <div className="dasboard-container">
+        <div className="filter-section">filter section</div>
+        <div className="product-list-section">
+          {productItem.map((item,index)=>{
+            return(
+              <ProductCard key={index} item={item}/>
+          )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProductCard = ({item}) => {
+  return (
+    <>
+      <div className="card-div">
+        <div className="product-name"><h3>{item.productName}</h3></div>
+        <div className="product-img"><img src={item.productImg} alt="" /></div>
+        <div className="product-desc">{item.productDescription}</div>
+
+        <div className="product-price"><h4><span className={item.rating>4?"product-rating-green":item.rating<2?"product-rating-red":"product-rating-yellow"}>{item.rating}</span> &#8377;{item.price}</h4></div>
+        <div className="btn-div">
+            <button className='view-details'>View Details</button>
+            <button className='add-to-cart'>Add to Cart</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ProductCard
