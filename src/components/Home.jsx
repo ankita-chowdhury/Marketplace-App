a5ae40645057d582ee7ff1c9d8f4c668bd64a8c6
ankita-chowdhury <108832208+ankita-chowdhury@users.noreply.github.com>
2024-08-18 20:46:21 +0530

    implemented add-item section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,6 +12,20 @@ const Home = () => {
     const[userDetails,setUserDetails]=useState({});
     const[showAddItem,setShowAddItem]=useState(false);
     const[modalShow,setModalShow]=useState(false);
+    const[fetchAgain,setFetchAgain]=useState(false);
+    const[addItemData,setAddItemData]=useState({
+      productId: "",
+      productName: "",
+      productImg: "",
+      productDescription: "",
+      price: null,
+      category: "",
+      rating: 0.0,
+      sellerId: "",
+      buyerId: null,
+      soldFlag: false,
+      productListingDate: "",
+    })
     useEffect(()=>{
         const loginKey=localStorage.getItem('loginKey');
         if(!loginKey){
@@ -22,12 +36,13 @@ const Home = () => {
         const parsedUser = JSON.parse(loginKey);
         getUserDetails(parsedUser);
         filterItems("productList","");
-      },[])
+      },[fetchAgain])
 
       const productApiCall = async() =>{
         try{
           const response = await axios.get(`http://localhost:4500/products`);
           setProductItem(response.data);
+          setFetchAgain(false);
         }
         catch(e){
           console.log(e);
@@ -94,6 +109,56 @@ const Home = () => {
         }
       }
 
+      const handleAddChange = (inputVal,inputName) =>{
+          if(inputName==='item-name'){
+            setAddItemData((oldItems)=>{
+              return{...oldItems,productName:inputVal}
+            })
+          }
+          else if(inputName==='item-price'){
+            setAddItemData((oldItems)=>{
+              return{...oldItems,price:inputVal}
+            })
+          }
+          else if(inputName==='item-price'){
+            setAddItemData((oldItems)=>{
+              return{...oldItems,price:inputVal}
+            })
+          }
+          else if(inputName==="product-category"){
+            setAddItemData((oldItems)=>{
+              return{...oldItems,category:inputVal}
+            })
+          }
+          else if(inputName==='item-desc'){
+            setAddItemData((oldItems)=>{
+              return{...oldItems,productDescription:inputVal}
+            })
+          }
+          else if(inputName==='item-img'){
+            setAddItemData((oldItems)=>{
+              return{...oldItems,productImg:inputVal}
+            })
+          }
+        const date=new Date();
+        const fomattedDate = `${String(date.getDate()).padStart(2, '0')}-${String(date.getMonth() + 1).padStart(2, '0')}-${date.getFullYear()}`;
+        setAddItemData((oldItems)=>{
+          return{...oldItems,sellerId:userDetails.id,productListingDate:fomattedDate}
+        })
+
+      }
+
+      const handleAddItem = () =>{
+          const response= axios.post(`http://localhost:4500/products`,addItemData)
+          .then((response)=>{
+              setModalShow(false);
+              setFetchAgain(true);
+          })
+          .catch((e)=>{
+            console.log(e);
+          })
+      }
+
   return (
     <div className='box-container'>
       <Navbar/>
@@ -136,15 +201,15 @@ const Home = () => {
             <div className="modal-body">
               <label htmlFor="item-name">Product Name</label>
               <div>
-                <input type="text" id='item-name' placeholder='Enter product name'/>
+                <input type="text" id='item-name' placeholder='Enter product name' onChange={(e)=>handleAddChange(e.target.value,'item-name')}/>
               </div>
               <label htmlFor="item-price">Product Price</label>
               <div>
-                <input type="text" id='item-price' placeholder='Enter product price'/>
+                <input type="text" id='item-price' placeholder='Enter product price' onChange={(e)=>handleAddChange(e.target.value,'item-price')}/>
               </div>
               <div className='select-category'>
                 <label htmlFor="product-category">Product Category: </label>
-                <select name="" id="product-category">
+                <select name="product-category" id="product-category" onChange={(e)=>handleAddChange(e.target.value,"product-category")}>
                   <option value="Electronics">Electronics</option>
                   <option value="Clothes">Clothes</option>
                   <option value="Audio Product">Audio Product</option>
@@ -156,14 +221,14 @@ const Home = () => {
               </div>
               <label htmlFor="item-desc">Product Description</label>
               <div>
-                <textarea name="" id='item-desc' placeholder='Enter product description...'/>
+                <textarea name='item-desc' id='item-desc' placeholder='Enter product description...' onChange={(e)=>handleAddChange(e.target.value,'item-desc')}/>
               </div>
               <label htmlFor="item-img">Product Image</label>
               <div>
-                <input type="text" id='item-img' placeholder='Enter product image-url'/>
+                <input type="text" id='item-img' placeholder='Enter product image-url' onChange={(e)=>handleAddChange(e.target.value,'item-img')}/>
               </div>
               <div className="add-item-form-btn">
-                <button className='add-btn-save'>Add</button>
+                <button className='add-btn-save' onClick={()=>handleAddItem()}>Add</button>
                 <button className='add-btn-cancel'>Cancel</button>
               </div>
             </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({item}) => {
         <div className="product-img"><img src={item.productImg} alt="" /></div>
         <div className="product-desc">{item.productDescription}</div>
 
-        <div className="product-price"><h4><span className={item.rating>4?"product-rating-green":item.rating<2?"product-rating-red":"product-rating-yellow"}>{item.rating}</span> &#8377;{item.price}</h4></div>
+        <div className="product-price"><h4>{item.rating>0 &&<span className={item.rating>4?"product-rating-green":item.rating<2?"product-rating-red":"product-rating-yellow"}>{item.rating}</span>} &#8377;{item.price}</h4></div>
         <div className="btn-div">
             <button className='view-details'>View Details</button>
             <button className='add-to-cart'>Add to Cart</button>
