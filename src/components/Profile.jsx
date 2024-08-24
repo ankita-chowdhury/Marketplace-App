3859bd5ddc2c8536d3f140e33fec4f0acceb5cbb
ankita-chowdhury <108832208+ankita-chowdhury@users.noreply.github.com>
2024-08-24 12:46:41 +0530

    implemented edit profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -45,6 +45,66 @@ const Profile = () => {
   const handleEditEnable = () =>{
     setEditEnable(false);
   }
+
+  const updateUserData = (inputVal,inputName) =>{
+      if(inputName==='username'){
+        setUserData((oldval)=>{
+          return{...oldval,username:inputVal};
+        })
+      }
+      else if(inputName==='pass'){
+        setUserData((oldval)=>{
+          return{...oldval,password:inputVal};
+        })
+      }
+      else if(inputName==='dob'){
+        setUserData((oldval)=>{
+          return{...oldval,dateOfBirth:inputVal};
+        })
+      }
+      else if(inputName==='address-1'){
+        setUserData((oldval)=>{
+          return{...oldval,address:{...oldval.address,adressLine1:inputVal}};
+        })
+      }
+      else if(inputName==='city'){
+        setUserData((oldval)=>{
+          return{...oldval,address:{...oldval.address,city:inputVal}};
+        })
+      }
+      else if(inputName==='state'){
+        setUserData((oldval)=>{
+          return{...oldval,address:{...oldval.address,state:inputVal}};
+        })
+      }
+      else if(inputName==='country'){
+        setUserData((oldval)=>{
+          return{...oldval,address:{...oldval.address,country:inputVal}};
+        })
+      }
+      else if(inputName==='zip-code'){
+        setUserData((oldval)=>{
+          return{...oldval,address:{...oldval.address,zipCode:inputVal}};
+        })
+      }
+      else if(inputName==='contact-no'){
+        setUserData((oldval)=>{
+          return{...oldval,contactNo:inputVal};
+        })
+      }
+  }
+
+  const saveUserProfile = () => {
+    axios.put(`${BASE_URL}/users/${userData.id}`,userData)
+    .then((res)=>{
+        console.log("response save profile",res.data);
+        setEditEnable(true);
+    })
+    .catch((e)=>{
+      console.log(e);
+    })
+  }
+
   return (
     <div className='box-container'>
       <Navbar/>
@@ -55,19 +115,20 @@ const Profile = () => {
           <hr />
           <label htmlFor="username">Username</label>
           <div>
-            <input type="text" id='username' value={userData.username} disabled={editEnable} name='username' placeholder='Update your username'/>
+            <input type="text" id='username' value={userData.username} disabled={editEnable} name='username' onChange={(e)=>updateUserData(e.target.value,'username')} placeholder='Update your username'/>
           </div>
           <label htmlFor="email">Email</label>
           <div>
-            <input type="email" id='email' value={userData.email} disabled={editEnable} name='email' placeholder='Update your email'/>
+            <input type="email" id='email' value={userData.email} disabled={true} name='email' placeholder='Email update is not available'/>
           </div>
           <label htmlFor="pass">Password</label>
           <div>
-            <input type={editEnable?"password":"text"} id='pass' value={userData.password} disabled={editEnable} name='pass' placeholder='Update your password'/>
+            <input type={editEnable?"password":"text"} id='pass' value={userData.password} disabled={editEnable} name='pass'
+            onChange={(e)=>updateUserData(e.target.value,'pass')} placeholder='Update your password'/>
           </div>
           <label htmlFor="dob">Date-of-Birth</label>
           <div className='dob-div'>
-            <input type="date" id='dob' value={userData.dateOfBirth} disabled={editEnable} name='dob' placeholder='Update your Date of Birth'/>
+            <input type="date" id='dob' value={userData.dateOfBirth} disabled={editEnable} name='dob' onChange={(e)=>updateUserData(e.target.value,'dob')} placeholder='Update your Date of Birth'/>
           </div>
           <h4 className='address-section'>Address <span className='dropdown-img' onClick={()=>handleDropDown()}><img src={Dropdown} alt="" className={showAddress?"dropdown-hide":"dropdown-show"} /></span></h4>
           {showAddress && <>
@@ -75,32 +136,33 @@ const Profile = () => {
           <div className='address-div'>
             <label htmlFor="address-1">Address Line 1</label>
             <div>
-              <input type="text" id='address-1' value={userData.address.adressLine1} disabled={editEnable} name='address-1' placeholder='Enter Your Address'/>
+              <input type="text" id='address-1' value={userData.address.adressLine1} disabled={editEnable} name='address-1'
+              onChange={(e)=>updateUserData(e.target.value,'address-1')} placeholder='Enter Your Address'/>
             </div>
             <label htmlFor="city">City</label>
             <div>
-              <input type="text" id='city' value={userData.address.city} disabled={editEnable} name='city' placeholder='Enter your city name'/>
+              <input type="text" id='city' value={userData.address.city} disabled={editEnable} name='city' onChange={(e)=>updateUserData(e.target.value,'city')} placeholder='Enter your city name'/>
             </div>
             <label htmlFor="state">State</label>
             <div>
-              <input type="text" id='state' value={userData.address.state} disabled={editEnable} name='state' placeholder='Enter your State'/>
+              <input type="text" id='state' value={userData.address.state} disabled={editEnable} name='state' onChange={(e)=>updateUserData(e.target.value,'state')} placeholder='Enter your State'/>
             </div>
             <label htmlFor="country">Country</label>
             <div>
-              <input type="text" id='country' value={userData.address.country} disabled={editEnable} name='country' placeholder='Enter your Country'/>
+              <input type="text" id='country' value={userData.address.country} disabled={editEnable} name='country' onChange={(e)=>updateUserData(e.target.value,'country')} placeholder='Enter your Country'/>
             </div>
             <label htmlFor="zip-code">Zip-Code</label>
             <div>
-              <input type="number" id='zip-code' value={userData.address.zipCode} disabled={editEnable} name='zip-code' placeholder='Enter your Zip-Code'/>
+              <input type="number" id='zip-code' value={userData.address.zipCode} disabled={editEnable} name='zip-code' onChange={(e)=>updateUserData(e.target.value,'zip-code')} placeholder='Enter your Zip-Code'/>
             </div>
           </div>
           </>}
           <label htmlFor="contact-no">Contact No</label>
           <div>
-            <input type="number" id='contact-no' value={userData.contactNo} disabled={editEnable} name='contact-no' placeholder='Enter your Contact No' />
+            <input type="number" id='contact-no' value={userData.contactNo} disabled={editEnable} name='contact-no' onChange={(e)=>updateUserData(e.target.value,'contact-no')} placeholder='Enter your Contact No' />
           </div>
           {!editEnable && <div className='save-profile-div'>
-          <button className='save-profile'>Save</button>
+          <button className='save-profile' onClick={()=>saveUserProfile()}>Save</button>
           </div>}
         </div>        
       </div>
